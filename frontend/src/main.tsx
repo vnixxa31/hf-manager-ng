43627d403b3504f03633c228acd41a3f3b0f2505
Violet Nixxa <violet@nixxa.net>
2026-04-07 03:25:01 -0400

    add sonner

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
+import { Toaster } from "@/components/ui/sonner"
+
 import "./index.css"
 import App from "./App.tsx"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
@@ -9,6 +11,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
       <App />
+      <Toaster />
     </ThemeProvider>
   </StrictMode>
 )
